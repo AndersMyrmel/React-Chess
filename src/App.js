882b4d17fee9b3a6c54a40f35fbedde4420bf631
Anders <andersmyrmel@gmail.com>
2022-07-13 23:39:46 +0200

    removed redundant lines

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import { useState } from "react";
-import { Chessboard } from "react-chessboard";
-import { Chess } from 'chess.js'
 import {PlayRandomMove} from "./RandomGame"
 
 function App() {
