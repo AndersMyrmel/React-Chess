6d0054e53eec04d5e9641e743b9a45e1a6688f2d
Anders <andersmyrmel@gmail.com>
2022-07-13 23:37:27 +0200

    added play vs random moves

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { useState } from "react";
 import { Chessboard } from "react-chessboard";
+import { Chess } from 'chess.js'
+import {PlayRandomMove} from "./RandomGame"
 
 function App() {
+
   return (
     <div id='background'>
       <div id='board'>
-        <Chessboard id="BasicBoard" boardWidth={750}/>
+        <PlayRandomMove/>
       </div>
     </div>
   );
