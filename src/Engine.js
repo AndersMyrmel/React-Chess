1598dc295f48d07bec0f0fce5a42b94b0cb6ed18
Anders <andersmyrmel@gmail.com>
2022-08-02 20:39:00 +0200

    Added play vs engine functionality

--- src/Engine.js
+++ src/Engine.js
@@ -1,14 +1,48 @@
-import { Engine } from 'node-uci';
+import React, {useState} from 'react';
+import { Game, move, status, moves, aiMove, getFen } from 'js-chess-engine';
+import { Chessboard } from "react-chessboard";
+import { Chess } from 'chess.js';
 
 export function ChessEngine(){
-    const engine = new Engine('engine/executable/path')
-    engine
-      .chain()
-      .init()
-      .setoption('MultiPV', 3)
-      .position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq - 3 3')
-      .go({ depth: 15 })
-      .then(result => {
-        console.log(result)
-      })
+  const [visualGame, setVisualGame] = useState(new Chess());
+  const [game, setGame] = useState(new Game());
+
+  function safeGameMutate(modify) {
+    setVisualGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+
+  function onDrop(sourceSquare, targetSquare) {
+    let visualMove = null;
+    safeGameMutate((game) => {
+      visualMove = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q',
+      });
+    });
+    if (visualMove === null) return false; // Illegal move
+
+    game.move(sourceSquare, targetSquare);
+
+    let computerMove = game.aiMove(3);
+    let computerFrom = Object.keys(computerMove).toString().toLowerCase();
+    let computerTo = Object.values(computerMove).toString().toLowerCase();
+    
+    setGame(game);
+
+    safeGameMutate((visualGame) => {
+      visualGame.move({from: computerFrom,
+          to: computerTo});
+    })
+    return true;
+  }
+
+  if (visualGame.game_over() || visualGame.in_draw()) // Move this 
+      return console.log('Game over'); // exit if the game is over
+
+  return <Chessboard position={visualGame.fen()} onPieceDrop={onDrop} />;
 }
