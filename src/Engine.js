3a9eaf60a13eb1008c3b13a774ec5dc09d71945e
Anders <andersmyrmel@gmail.com>
2022-08-04 21:26:43 +0200

    added button to play next computer move

--- src/Engine.js
+++ src/Engine.js
@@ -3,9 +3,23 @@ import { Game, move, status, moves, aiMove, getFen } from 'js-chess-engine';
 import { Chessboard } from "react-chessboard";
 import { Chess } from 'chess.js';
 
+const FEN = '6k1/3qb1pp/4p3/ppp1P3/8/2PP1Q2/PP4PP/5RK1 w - - 0 1'
+
 export function ChessEngine(){
-  const [visualGame, setVisualGame] = useState(new Chess());
-  const [game, setGame] = useState(new Game());
+  const [visualGame, setVisualGame] = useState(new Chess(FEN));
+  const [game, setGame] = useState(new Game(FEN));
+
+  const handleClick = () => {
+    let computerMove = game.aiMove(3);
+    let computerFrom = Object.keys(computerMove).toString().toLowerCase();
+    let computerTo = Object.values(computerMove).toString().toLowerCase();
+    setGame(game);
+    safeGameMutate((visualGame) => {
+      visualGame.move({from: computerFrom,
+          to: computerTo});
+    })
+    console.log(computerMove);
+  };
 
   function safeGameMutate(modify) {
     setVisualGame((g) => {
@@ -27,13 +41,10 @@ export function ChessEngine(){
     if (visualMove === null) return false; // Illegal move
 
     game.move(sourceSquare, targetSquare);
-
     let computerMove = game.aiMove(3);
     let computerFrom = Object.keys(computerMove).toString().toLowerCase();
     let computerTo = Object.values(computerMove).toString().toLowerCase();
-    
     setGame(game);
-
     safeGameMutate((visualGame) => {
       visualGame.move({from: computerFrom,
           to: computerTo});
@@ -44,5 +55,10 @@ export function ChessEngine(){
   if (visualGame.game_over() || visualGame.in_draw()) // Move this 
       return console.log('Game over'); // exit if the game is over
 
-  return <Chessboard position={visualGame.fen()} onPieceDrop={onDrop} />;
+  return (
+    <div><Chessboard position={visualGame.fen()} onPieceDrop={onDrop} />
+    <button onClick={handleClick}>
+    Next Move
+      </button>
+    </div>)
 }
