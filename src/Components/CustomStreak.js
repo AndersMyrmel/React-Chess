9a95485bdf19a29a365a1957047ca846d326ae22
Anders <andersmyrmel@gmail.com>
2022-08-07 00:05:59 +0200

    Refactored code

--- src/Components/CustomStreak.js
+++ src/Components/CustomStreak.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Game, move, status, moves, aiMove, getFen } from "js-chess-engine";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
+import { getBestMove } from "../services/getBestMove.js";
 import "../Engine/lozza.js";
 
 const FEN = "6k1/3qb1pp/4p3/ppp1P3/8/2PP1Q2/PP4PP/5RK1 w - - 0 1";
@@ -9,26 +10,6 @@ const FEN = "6k1/3qb1pp/4p3/ppp1P3/8/2PP1Q2/PP4PP/5RK1 w - - 0 1";
 export function CustomStreak() {
 	const [visualGame, setVisualGame] = useState(new Chess(FEN));
 	const [game, setGame] = useState(new Game(FEN));
-
-	// Parse result data from lozza engine to four digit string
-	function getLastDigits(str) {
-		return str.split("bestmove")[1].trim();
-	}
-
-	// Calculate the best move in a given fen position using the lozza engine
-	function getBestMove(fen) {
-		let lozza = new Worker("/lozza.js"); // Inititate web worker, communicating using the UCI protocol
-		lozza.postMessage("position fen " + fen); // Feed fen position
-		lozza.postMessage("go depth 14"); // 14 Ply search
-
-		return new Promise((resolve) => {
-			lozza.onmessage = function (e) {
-				if (e.data.includes("bestmove")) {
-					resolve(getLastDigits(e.data));
-				}
-			};
-		});
-	}
 
 	async function handleClick() {
 		let bestMove = await getBestMove(FEN);
