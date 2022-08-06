50e1511f88af7017cd7c168e1f2b06b45af2ffec
Anders <andersmyrmel@gmail.com>
2022-08-07 00:30:41 +0200

    Refactored code

--- src/services/getBestMove.js
+++ src/services/getBestMove.js
@@ -1,15 +1,15 @@
-import { getLastDigits } from "./getLastDigits.js";
+import { getLastDigits } from './getLastDigits.js';
 
 // Calculate the best move in a given fen position using the lozza engine
 export const getBestMove = (fen) => {
-  let lozza = new Worker("/lozza.js"); // Inititate web worker, communicating using the UCI protocol
-  lozza.postMessage("position fen " + fen); // Feed fen position
-  lozza.postMessage("go depth 14"); // 14 Ply search
-  return new Promise((resolve) => {
-    lozza.onmessage = function (e) {
-      if (e.data.includes("bestmove")) {
-        resolve(getLastDigits(e.data));
-      }
-    };
-  });
+	let lozza = new Worker('/lozza.js'); // Inititate web worker, communicating using the UCI protocol
+	lozza.postMessage('position fen ' + fen); // Feed fen position
+	lozza.postMessage('go depth 14'); // 14 Ply search
+	return new Promise((resolve) => {
+		lozza.onmessage = function (e) {
+			if (e.data.includes('bestmove')) {
+				resolve(getLastDigits(e.data));
+			}
+		};
+	});
 };
--- src/services/getLastDigits.js
+++ src/services/getLastDigits.js
@@ -1,4 +1,4 @@
 //Parse result data from lozza engine to four digit string
 export const getLastDigits = (str) => {
-  return str.split("bestmove")[1].trim();
+	return str.split('bestmove')[1].trim();
 };
