c48fa343ef0de0b2854e85e2f4174afb3d6df717
Anders <andersmyrmel@gmail.com>
2022-08-07 04:25:11 +0200

    Added text to inputfen component

--- src/Components/InputFen.js
+++ src/Components/InputFen.js
@@ -4,51 +4,76 @@ import '../App.css';
 
 export const InputFen = () => {
 	const [fields, setFields] = useState(['']);
+	const [count, setCount] = useState(1);
 
-	function handleChange(i, event) {
+	const handleChange = (i, event) => {
 		const values = [...fields];
 		values[i] = event.target.value;
 		setFields(values);
-	}
+	};
 
-	function handleAdd() {
+	const handleAdd = () => {
+		if (count > 9) {
+			alert('Max ten entries');
+			return;
+		}
 		const values = [...fields];
 		values.push('');
 		setFields(values);
-	}
+		setCount(count + 1);
+	};
 
 	const submitHandler = (event) => {
 		event.preventDefault();
+		console.log(fields);
 	};
 
 	return (
-		<div className="inputContainer">
-			<form onSubmit={submitHandler}>
-				{fields.map((field, idx) => {
-					return (
-						<div className="form-inline" key={`${idx}`}>
-							<Input
-								placeholder="Enter FEN notation"
-								value={field || ''}
-								sx={{
-									':before': { borderBottomColor: 'white' },
-									':after': { borderBottomColor: '#779556' },
-								}}
-								onChange={(e) => handleChange(idx, e)}
-								style={inputStyle}
-							/>
-						</div>
-					);
-				})}
-				<div className="button-section">
-					<button id="addButton" onClick={() => handleAdd()}>
-						Add
-					</button>
-					<button id="submitButton" type="submit">
-						Submit
-					</button>
-				</div>
-			</form>
+		<div>
+			<div id="inputText">
+				<h1 id="inputHeader">Create your own puzzle streak!</h1>
+				<p>
+					Paste FEN notations in the format: 8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1
+					to get started.
+				</p>
+
+				<p>
+					Click add to increase the number of input fields, and submit when you
+					are satisfied with the number of puzzles.
+				</p>
+				<p>
+					Make sure it's the notation for the starting layout of the puzzle.
+				</p>
+			</div>
+
+			<div className="inputContainer">
+				<form onSubmit={submitHandler}>
+					{fields.map((field, idx) => {
+						return (
+							<div className="form-inline" key={`${idx}`}>
+								<Input
+									placeholder="Enter FEN"
+									value={field || ''}
+									sx={{
+										':before': { borderBottomColor: 'white' },
+										':after': { borderBottomColor: '#779556' },
+									}}
+									onChange={(e) => handleChange(idx, e)}
+									style={inputStyle}
+								/>
+							</div>
+						);
+					})}
+					<div className="button-section">
+						<button id="addButton" onClick={() => handleAdd()}>
+							Add
+						</button>
+						<button id="submitButton" type="submit">
+							Submit
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
