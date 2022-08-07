1f44b44458301577861fe75bcf121984e4f45582
Anders <andersmyrmel@gmail.com>
2022-08-07 22:36:19 +0200

    Added useMemo hook for fen context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { CustomStreak } from './Components/CustomStreak.js';
 import { InputFen } from './Components/InputFen.js';
@@ -7,8 +7,9 @@ import './App.css';
 
 const App = () => {
 	const [value, setValue] = useState([]);
+	const providerValue = useMemo(() => [value, setValue], [value, setValue]);
 	return (
-		<FenContext.Provider value={[value, setValue]}>
+		<FenContext.Provider value={providerValue}>
 			<Routes>
 				<Route path="/" element={<InputFen />} />
 				<Route path="CustomStreak" element={<CustomStreak />} />
