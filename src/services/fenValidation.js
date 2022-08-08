69006a66665b16d41fecfbfb60afc7b76d9e8006
Anders <andersmyrmel@gmail.com>
2022-08-08 03:52:46 +0200

    Made comments clearer

--- src/services/fenValidation.js
+++ src/services/fenValidation.js
@@ -1,4 +1,4 @@
-// Regular expression to validate wheter a given string is a valid FEN notation
+// Match string to regular expression to validate wheter the given string is a valid FEN notation
 export const validateFen = (str) => {
 	const pattern =
 		'\\s*([rnbqkpRNBQKP1-8]+\\/){7}([rnbqkpRNBQKP1-8]+)\\s[bw-]\\s(([a-hkqA-HKQ]{1,4})|(-))\\s(([a-h][36])|(-))\\s\\d+\\s\\d+\\s*';
